8b6296c1bf43066c5a9c77cb3c06662a9fc4a3f7
nick0la85 <nicdimarco@rocketmail.com>
2023-04-02 19:27:58 +0200

    first commit

---
+++ W1-P2.js
@@ -0,0 +1,62 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+
+/* ESERCIZIO 1
+ let risposta1= "I datatype princiali in Javascript sono: numeri, stringhe, boolean,null, undefined, oggetti, funzioni e array. i numeri rappresentano un valore di tipo numerico 1,2, ecc., le stringhe ci permettono delle lettere e sono racchiuse in virgolette, boolean invece rappresenta il valore vero o falso, null invece indica l'assenza din un valore nella variabile nonostante sia stata dichiarta, undefined indica che alla variabile non è stato dichiarato alcun valore, "
+
+/* */
+
+/* ESERCIZIO 2
+ In javascript un oggetto
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 3
+ Scriti il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+
+/* let somma =(12+20)*/
+ 
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+
+ /*let x=12 */
+
+/* ESERCIZIO 5
+ Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+
+/* let name = "Nicola"*/
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+
+/*let sottrazione = (4-x) per funzionare bisogna decommnetare risposta esrcizio 4*/
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ Infine, verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+ NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
+*/
+
+/* let name1 = "john"
+let name2= "John";
+console.log (name1 !== name2);
+console.log (name1===name2.toLowerCase())
+)*/
